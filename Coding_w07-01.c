e52f2c970538e5d3ada98fe04f8ec0706e5e1790
Panithi-6812022611286 <s6812022611286@email.kmutnb.ac.th>
2025-08-26 20:53:13 +0700

    Update Coding_w07-01.c

--- Coding_w07-01.c
+++ Coding_w07-01.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-int main() {
+int main() { 
 
-    float score;
-    float score_plus;
+    float score; //ประกาศตัวแปร score
+    float score_plus; //ประกาศตัวแปร score_plus เพื้อใช้ในการคำนวณ
 
-    printf("Please enter your score : ");
-    scanf("%f", &score);
+    printf("Please enter your score : "); 
+    scanf("%f", &score); //แสดงข้อความให้ผุ็ใช้ใส่ค่า คะแนน
     
     if (score >= 50) {
-        score_plus = score + (score*0.05);
+        score_plus = score + (score*0.05); //ถ้าคะแนนมากกว่าหรือเท้ากับ 50 ให้นำคะแนนไปคำนวณบวกคะแนนเพิ่มไปอีก 5%
     } else {
-        score_plus = score;
+        score_plus = score; //ถ้าคะแนนน้อยกว่า 50 จะไม่ได้การบวกคะแนนเพิ่ม
     }
 
-    printf("Your final score is : %.2f\n", score_plus);
-    printf("End of evaluation\n");
+    printf("Your final score is : %.2f\n", score_plus); //แสดงผลคะแนนหลังจากการคำนวณแล้ว
+    printf("End of evaluation\n"); //แสดงข้อความจบการคำนวณ
 
     return 0;
+
 }
